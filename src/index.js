38f8cde72215255645157cb63e7f25c55c6081d4
jiayi <jyhee@nh2system.com>
2024-07-11 01:54:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,13 @@ import store from './redux-state/CartState';
 import { ChakraProvider } from '@chakra-ui/react';
 
 const root = render.createRoot(document.getElementById('root'));
+
+render(
+  <App/>,
+  document.getElementById('root')
+);
+
+/*
 root.render(
   <React.StrictMode>
     <ChakraProvider>
@@ -21,7 +28,7 @@ root.render(
     </ChakraProvider>
   </React.StrictMode>
 );
-
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
